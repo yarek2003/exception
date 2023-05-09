3efa9818dbc83de8db0ba4e44d5ccd703bfa646f
yarek <yarek2003@mail.ru>
2023-05-09 18:39:30 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+
 public class Main {
     public static void main(String[] args) {
+        String login = "login_A1";
+        String password = "password_A11111111111111110";
+        String confirmPassword = "password_A1*";
 
 
     }
+
 }
